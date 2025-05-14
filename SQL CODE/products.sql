7d488efde1389e282417beb35393eedee00d26c8
Jamil Mariano <marianojamil15@gmail.com>
2025-05-14 17:13:18 +0800

    update sql code for products table

--- SQL CODE/products.sql
+++ SQL CODE/products.sql
@@ -1,16 +1,14 @@
 CREATE TABLE `products` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `product_name` VARCHAR(255) DEFAULT NULL,
-  `product_price` DECIMAL(10,2) DEFAULT NULL,
-  `product_status` VARCHAR(50) DEFAULT NULL,
-  `quantity_small` INT(11) DEFAULT 0,
-  `quantity_medium` INT(11) DEFAULT 0,
-  `quantity_large` INT(11) DEFAULT 0,
-  `photoFront` VARCHAR(255) DEFAULT NULL,
-  `photo1` VARCHAR(255) DEFAULT NULL,
-  `photo2` VARCHAR(255) DEFAULT NULL,
-  `photo3` VARCHAR(255) DEFAULT NULL,
-  `photo4` VARCHAR(255) DEFAULT NULL,
-  `box_id` INT(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL,
+  `product_status` varchar(50) DEFAULT NULL,
+  `quantity_small` int(11) DEFAULT 0,
+  `quantity_medium` int(11) DEFAULT 0,
+  `quantity_large` int(11) DEFAULT 0,
+  `photoFront` varchar(255) DEFAULT NULL,
+  `photo1` varchar(255) DEFAULT NULL,
+  `photo2` varchar(255) DEFAULT NULL,
+  `photo3` varchar(255) DEFAULT NULL,
+  `photo4` varchar(255) DEFAULT NULL
+)
